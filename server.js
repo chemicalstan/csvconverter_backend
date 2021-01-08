a4533b4acfce7796f858bd4839b4ade15fabece8
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-08 12:40:20 +0100

    Install joi validation module

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const _ = require("lodash");
 const { v4: uuidv4 } = require("uuid");
 const csv = require("csvtojson");
 const request = require("request");
+const joi = require("joi");
 
 const port = "3999";
 const app = express();
@@ -13,30 +14,45 @@ app.get("/", (req, res) => {
 });
 
 // HTTP POST end point
-app.post("/", async (req, res) => {
-  const { url, select_fields } = req.body.csv;
+app.post(
+  "/",
+  (req, res, next) => {
+    
+  },
+  async (req, res) => {
+    console.log(req.body.constructor);
+    console.log(Object.keys(req.body));
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+      // console.log();
+    }
 
-  const arr = [];
+    try {
+      const { url, select_fields } = req.body.csv;
 
-  const jsonArray = await csv().fromStream(request.get(url));
+      const jsonArray = await csv().fromStream(request.get(url));
 
-  // jsonArray
-  if (!select_fields) {
-    res.status(200).json({
-      conversion_key: uuidv4(),
-      json: jsonArray
-    });
+      if (!select_fields) {
+        res.status(200).json({
+          conversion_key: uuidv4(),
+          json: jsonArray
+        });
+      }
+
+      const result = jsonArray.map(obj => {
+        return _.pick(obj, select_fields);
+      });
+
+      res.status(200).json({
+        conversion_key: uuidv4(),
+        json: result
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Ops, something went wrong."
+      });
+    }
   }
-
-  const result = jsonArray.map(obj => {
-    return _.pick(obj, select_fields);
-  });
-
-  res.status(200).json({
-    conversion_key: uuidv4(),
-    json: result
-  });
-});
+);
 
 app.listen(port, () => {
   console.log(`server running at ${port}`);
